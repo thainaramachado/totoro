b0541c1295b3d45e6edee1d7e2994c8bb31f09a5
thainaramachado <dev.thainara@gmail.com>
2026-02-13 16:46:44 -0300

    Create Category and Transaction models

--- finances/models.py
+++ finances/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Transaction(models.Model):
+
+    TRANSACTION_TYPES = [
+        ('income', 'Entrada'),
+        ('expense', 'Saída'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
