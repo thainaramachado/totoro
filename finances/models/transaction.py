51650644742a3f53961257778c2b9acd0787a15e
thainaramachado <dev.thainara@gmail.com>
2026-02-18 16:54:46 -0300

    Refactor models to package structure

--- finances/models/transaction.py
+++ finances/models/transaction.py
@@ -1,14 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
-# Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+from .category import Category
     
 class Transaction(models.Model):
 
